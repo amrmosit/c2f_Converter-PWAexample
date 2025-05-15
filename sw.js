827c39bbf99a58cfd4ca82aa2f65921194deae22
Amr Mostafa <amrmos.it@gmail.com>
2025-05-15 15:27:09 +1000

    Adding event listner for fetch event

--- sw.js
+++ sw.js
@@ -15,3 +15,32 @@ self.addEventListener('install', event =>{
     }));
 });
 // Even listner for the 'fetch' event, triggered for each network request made by the page
+self.addEventListener('fetch', event =>{
+    event.respondWith((async()=>{
+        // open cache to check for cashed resources
+        const cashe = await caches.open(CACHE_NAME);
+
+        try {
+            // Try to fetch the resources from the network
+            const fetchResponse = await fetch(event.request);
+            // If successful, store the network response in the cache for future use
+            cache.put (event.request, fetchResponse.clone());
+
+            // return the network response to the page
+            return fetch.fetchResponse;
+        } catch (e) {
+            // If the network request fails (e.g., offline), try to fetch from the cache
+            const cashedResponse = await cache.match(event.request);
+            // If the resource is found in the cashe, return it
+            if (cashedResponse){
+                return cashedResponse
+            } else {
+                // If the resource is not available in the cahe, return a customer error response
+                return new Response ('Network error occured and no cache available', {
+                    status:408, // Request Timeout status
+                    statusText: 'Network error occured'
+                });
+            }
+        }
+    }));
+});
