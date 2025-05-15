26fa9518c231ff640d0680892a54cc357002270c
Amr Mostafa <amrmos.it@gmail.com>
2025-05-15 16:28:42 +1000

    final commit; debugging the code - now it is working

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
 // Adding Service workers to convert the website to PWA
 const CACHE_NAME = 'c-to-f-converter';
-// Event listner for the 'install' event, triggered when the service wroker is first installed
+// Event listener for the 'install' event, triggered when the service worker is first installed
 self.addEventListener('install', event =>{
     event.waitUntil((async()=>{
         // Open (or Create) a cache with the specified name
         const cache = await caches.open(CACHE_NAME);
         // Add files to the cache for offline use
         await cache.addAll([
-            '/', //Add homebage
+            '/', //Add homepage
             '/script.js', //add Java script file
             '/style.css', // add CSS file
             '/bootstrap.min.css', // add CSS bootstrap file
         ]);
     }));
 });
-// Even listner for the 'fetch' event, triggered for each network request made by the page
+// Even listener for the 'fetch' event, triggered for each network request made by the page
 self.addEventListener('fetch', event =>{
     event.respondWith((async()=>{
         // open cache to check for cashed resources
@@ -31,16 +31,16 @@ self.addEventListener('fetch', event =>{
         } catch (e) {
             // If the network request fails (e.g., offline), try to fetch from the cache
             const cachedResponse = await cache.match(event.request);
-            // If the resource is found in the cashe, return it
+            // If the resource is found in the cache, return it
             if (cachedResponse){
                 return cachedResponse
             } else {
-                // If the resource is not available in the cache, return a customer error response
-                return new Response ('Network error occured and no cache available', {
+                // If the resource is not available in the cache, return a custom error response
+                return new Response ('Network error occurred and no cache available', {
                     status:408, // Request Timeout status
-                    statusText: 'Network error occured'
+                    statusText: 'Network error occurred'
                 });
             }
         }
-    }));
+    })());
 });
