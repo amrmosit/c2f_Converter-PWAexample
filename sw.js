31e139ce4092587712ff6130d028ed1c9d22ccd3
Amr Mostafa <amrmos.it@gmail.com>
2025-05-15 15:09:34 +1000

    Adding sw.js file and structure to convert the webpage to PWA

---
+++ sw.js
@@ -0,0 +1,7 @@
+// Adding Service workers to convert the website to PWA
+const CACHE_NAME = 'c-to-f-converter';
+// Event listner for the 'install' event, triggered when the service wroker is first installed
+
+
+
+// Even listner for the 'fetch' event, triggered for each network request made by the page
